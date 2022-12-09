7d1c67bbf134d2c2387af973f2ca0b5415285ac5
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2022-12-09 17:54:32 +0100

    feat(category app): added projectId in category entity with routes to delete or get by projectID

--- apps/category/category.module.ts
+++ apps/category/category.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectModule } from 'apps/project/project.module';
 import { CategoryController } from './category.controller';
 import { Category } from './category.entity';
 import { CategoryService } from './category.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category]), ProjectModule],
   providers: [CategoryService],
   controllers: [CategoryController],
   exports: [CategoryService],
--- apps/category/dto/create-category.dto.ts
+++ apps/category/dto/create-category.dto.ts
@@ -1,7 +1,12 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsUUID } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
   name!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  projectId!: string;
 }

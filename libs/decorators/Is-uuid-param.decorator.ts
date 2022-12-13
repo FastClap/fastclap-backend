69bcf18dd59b60b6a0d3c46c6c14077d78bbbe7e
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2022-12-13 15:59:29 +0100

    feat(IsUuidParam decorator & exception libs): fixed error handling return when param is undefined in isUuidParam decorator && added exception library functions to throw badRequest or notFound httpException

--- libs/decorators/Is-uuid-param.decorator.ts
+++ libs/decorators/Is-uuid-param.decorator.ts
@@ -1,21 +1,18 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { throwWrongFormat } from '../errors/InvalidFormat';
+import { BadRequestException } from '../exceptions/bad-request.exception';
 
 export const IsUuidParam = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const uuid: string = request.params[data];
     if (!uuid) {
-      return '';
+      return uuid;
     }
-    // this regexp insure the fact
-    const cmp = uuid
-      .slice()
-      .match(
-        '^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
-      );
+    const cmp = uuid.match(
+      '^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
+    );
     if (cmp === null) {
-      throw throwWrongFormat(data, 'uuid');
+      throw BadRequestException(data, 'uuid');
     }
     return uuid;
   },
